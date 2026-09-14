264dd6efca9b20914afc93de08679866cfac79db
rijulpaul <neutral.zfab4@proton.me>
2026-09-14 22:23:05 +0530

    i forgor

--- src/peft.py
+++ src/peft.py
@@ -1,18 +1,16 @@
 from peft import get_peft_model as gpm
 from src.config import get_config
+from collections import OrderedDict
 
 
 def get_peft_model(model):
-    peft_config = get_config().lora
-
     model = gpm(model, peft_config)
     model.print_trainable_parameters()
 
     return model
 
 def build_state_dict(state_dict):
-    config = get_config().lora
-    new_state_dict = {}
+    new_state_dict = OrderedDict()
     for k in state_dict:
         lk = k.split('.')
         if lk[-2]=="base_layer":
@@ -20,22 +18,21 @@ def build_state_dict(state_dict):
             base = k
             a = k.replace('base_layer','lora_A.default')
             b = k.replace('base_layer','lora_B.default')
-            weights = merge_lora(state_dict[base],state_dict[a],state_dict[b],config.alpha,config.rank)
+            weights = merge_lora(state_dict[base],state_dict[a],state_dict[b])
             nk = k.replace('base_model.model.','')    # Remove 'base_model.model' prefix
             nk = nk.replace('base_layer.','')         # Remove 'base_model.model' prefix
-            new_state_dict[k] = weights
+            new_state_dict[nk] = weights
         elif lk[-3] in ["lora_A","lora_B"]:
             # Pray to God we identified the base_layer and executed the merge
             continue
         else:
             nk = k.replace('base_model.model.','')    # Remove 'base_model.model' prefix
             new_state_dict[nk] = state_dict[k]
-        del state_dict[k]
 
     return new_state_dict
 
 
-def merge_lora(base_weight, lora_A, lora_B, alpha, rank):
+def merge_lora(base_weight, lora_A, lora_B, alpha=32, rank=16):
     scaling = alpha / rank
 
     # LoRA update
