d9de35073a033fe6ad798b399982d654050d4397
rijulpaul <neutral.zfab4@proton.me>
2026-09-15 09:47:08 +0530

    lora save checkpoint fix attempt#6

--- src/finetune.py
+++ src/finetune.py
@@ -1,6 +1,7 @@
 import os, shutil, json, time
 import numpy as np
 import soundfile as sf
+from peft import PeftModel
 
 from src.config import get_config
 from src.eval import calculate_speaker_similarity, calculate_word_error_rate
@@ -267,51 +268,55 @@ def finetune(model, train_dataset, test_dataset):
                 if config.lora:
                     # save adapters
                     unwrapped_model.save_pretrained(output_dir)
-                else:
-                    shutil.copytree(
-                        training_config.model_path, output_dir, dirs_exist_ok=True
-                    )
+                    unwrapped_model = Qwen3TTSModel.from_pretrained(
+                        training_config.model_path,
+                        device="auto"
+                    ).model
 
-                    input_config_file = os.path.join(
-                        training_config.model_path, "config.json"
-                    )
-                    output_config_file = os.path.join(output_dir, "config.json")
-                    with open(input_config_file, "r", encoding="utf-8") as f:
-                        config_dict = json.load(f)
-                        config_dict["tts_model_type"] = "custom_voice"
-                        talker_config = config_dict.get("talker_config", {})
-                        talker_config["spk_id"] = {training_config.speaker_name: 3000}
-                        talker_config["spk_is_dialect"] = {
-                            training_config.speaker_name: False
-                        }
-                        config_dict["talker_config"] = talker_config
+                shutil.copytree(
+                    training_config.model_path, output_dir, dirs_exist_ok=True
+                )
 
-                    with open(output_config_file, "w", encoding="utf-8") as f:
-                        json.dump(config_dict, f, indent=2, ensure_ascii=False)
-
-
-                    state_dict = {
-                        k: v.detach().to("cpu")
-                        for k, v in unwrapped_model.state_dict().items()
+                input_config_file = os.path.join(
+                    training_config.model_path, "config.json"
+                )
+                output_config_file = os.path.join(output_dir, "config.json")
+                with open(input_config_file, "r", encoding="utf-8") as f:
+                    config_dict = json.load(f)
+                    config_dict["tts_model_type"] = "custom_voice"
+                    talker_config = config_dict.get("talker_config", {})
+                    talker_config["spk_id"] = {training_config.speaker_name: 3000}
+                    talker_config["spk_is_dialect"] = {
+                        training_config.speaker_name: False
                     }
+                    config_dict["talker_config"] = talker_config
+
+                with open(output_config_file, "w", encoding="utf-8") as f:
+                    json.dump(config_dict, f, indent=2, ensure_ascii=False)
 
 
-                    drop_prefix = "speaker_encoder"
-                    keys_to_drop = [
-                        k for k in state_dict.keys() if k.startswith(drop_prefix)
-                    ]
-                    for k in keys_to_drop:
-                        del state_dict[k]
+                state_dict = {
+                    k: v.detach().to("cpu")
+                    for k, v in unwrapped_model.state_dict().items()
+                }
 
-                    weight = state_dict["talker.model.codec_embedding.weight"]
-                    state_dict["talker.model.codec_embedding.weight"][3000] = (
-                        target_speaker_embedding[0]
-                        .detach()
-                        .to(weight.device)
-                        .to(weight.dtype)
-                    )
-                    save_path = os.path.join(output_dir, "model.safetensors")
-                    save_file(state_dict, save_path)
+
+                drop_prefix = "speaker_encoder"
+                keys_to_drop = [
+                    k for k in state_dict.keys() if k.startswith(drop_prefix)
+                ]
+                for k in keys_to_drop:
+                    del state_dict[k]
+
+                weight = state_dict["talker.model.codec_embedding.weight"]
+                state_dict["talker.model.codec_embedding.weight"][3000] = (
+                    target_speaker_embedding[0]
+                    .detach()
+                    .to(weight.device)
+                    .to(weight.dtype)
+                )
+                save_path = os.path.join(output_dir, "model.safetensors")
+                save_file(state_dict, save_path)
 
             # --- upload checkpoints to Hugging Face Hub ---
             for ckpt_config in [model_ckpt_config, train_ckpt_config]:
@@ -332,44 +337,29 @@ def finetune(model, train_dataset, test_dataset):
 
             if config.testing and test_dataset:
                 # Load the model checkpoint and test
+                output_dir = os.path.join(
+                    model_ckpt_config.output_path.format(
+                        epoch=f"{epoch:03d}", global_step=global_step
+                    )
+                )
+                tts = Qwen3TTSModel.from_pretrained(
+                    output_dir,
+                    device_map="auto",
+                    attn_implementation=training_config.attn_implementation,
+                )
                 if config.lora:
-                    tts = Qwen3TTSModel.from_pretrained(
-                        training_config.model_path.format(
-                            epoch=f"{epoch:03d}", global_step=global_step
-                        ),
-                        device_map="auto",
-                        attn_implementation=training_config.attn_implementation,
+                    tts.model = PeftModel.from_pretrained(
+                        tts.model,
+                        output_dir
                     )
-
-                    output_dir = os.path.join(
-                        model_ckpt_config.output_path.format(
-                            epoch=f"{epoch:03d}", global_step=global_step
-                        )
-                    )
-
-                    tts.model.add_adapter(output_dir)
-                else:
-                    tts = Qwen3TTSModel.from_pretrained(
-                        model_ckpt_config.output_path.format(
-                            epoch=f"{epoch:03d}", global_step=global_step
-                        ),
-                        device_map="auto",
-                        attn_implementation=training_config.attn_implementation,
-                    )
-
                 idx = 0
                 eval_log = {}
                 for example in test_dataset:
                     # use each test dataset transcript to generate audio.
-                    if config.lora:
-                        wavs, sr = tts.generate_voice_clone(
+                    wavs, sr = tts.generate_custom_voice(
                         text=example[config.dataset.transcript_column],
+                        speaker=training_config.speaker_name,
                     )
-                    else:
-                        wavs, sr = tts.generate_custom_voice(
-                            text=example[config.dataset.transcript_column],
-                            speaker=training_config.speaker_name,
-                        )
 
                     base_audio = example[config.dataset.audio_column]
                     generated_audio = {
